9dfa48de338baaa7e537b754621092b6482bf8d5
Johannes <johannes-programming@mailfence.com>
2024-08-03 01:03:24 +0200

    Version 0.0.2

--- src/rightness/__init__.py
+++ src/rightness/__init__.py
@@ -1,43 +1,31 @@
 class RightObject:
-    pass
-
-class _Setup:
-    @classmethod
-    def _get_rfunc(cls, l):
-        def ans(self, other):
-            c = type(self)
-            x = c(other)
-            if c is not type(x):
-                raise TypeError(x)
-            f = getattr(c, l)
-            z = f(x, self)
-            return z
-        return ans
-    @classmethod
-    def _rmagic(cls, lname, rname=None):
-        if rname is None:
-            rname = "r" + lname
-        lname = "__" + lname + "__"
-        rname = "__" + rname + "__"
-        rfunc = cls._get_rfunc(lname)
-        rfunc.__name__ = rname
-        setattr(RightObject, rname, rfunc)
-    @classmethod
-    def rmagics(cls):
-        cls._rmagic("add")
-        cls._rmagic("and")
-        cls._rmagic("divmod")
-        cls._rmagic("floordiv")
-        cls._rmagic("lshift")
-        cls._rmagic("mod")
-        cls._rmagic("mul")
-        cls._rmagic("or")
-        cls._rmagic("pow")
-        cls._rmagic("rshift")
-        cls._rmagic("sub")
-        cls._rmagic("truediv")
-        cls._rmagic("xor")
-        cls._rmagic("lt", "gt")
-        cls._rmagic("le", "ge")
-            
-_Setup.rmagics()
+    def __ge__(self, other):
+        return type(self)(other) <= self
+    def __gt__(self, other):
+        return type(self)(other) < self
+    def __radd__(self, other):
+        return type(self)(other) + self
+    def __rand__(self, other):
+        return type(self)(other) & self
+    def __rdivmod__(self, other):
+        return divmod(type(self)(other), self)
+    def __rfloordiv__(self, other):
+        return type(self)(other) // self
+    def __rlshift__(self, other):
+        return type(self)(other) << self
+    def __rmod__(self, other):
+        return type(self)(other) % self
+    def __rmul__(self, other):
+        return type(self)(other) * self
+    def __ror__(self, other):
+        return type(self)(other) | self
+    def __rpow__(self, other):
+        return type(self)(other) ** self
+    def __rrshift__(self, other):
+        return type(self)(other) >> self
+    def __rsub__(self, other):
+        return type(self)(other) - self
+    def __rtruediv__(self, other):
+        return type(self)(other) / self
+    def __rxor__(self, other):
+        return type(self)(other) ^ self
